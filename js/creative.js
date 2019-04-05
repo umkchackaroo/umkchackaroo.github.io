978110e9c78ae8dcaf0c534bbd5f2b0ee2628e50
umkchackaroo <44585348+umkchackaroo@users.noreply.github.com>
2019-04-05 09:33:15 -0500

    Set Grandfinale date

--- js/creative.js
+++ js/creative.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    setCountdownInfo(new Date('04/04/2019 5:30:00 PM'));
+    setCountdownInfo(new Date('04/15/2019 5:30:00 PM'));
     window.setInterval(function(){
         setCountdownInfo(new Date('04/04/2019 5:30:00 PM'));
     }, 1000);
